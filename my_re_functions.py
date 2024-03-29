227599b5238cf56d87c80cef080d4da62915b44c
George Tzanopoulos <giwrgos.tzanopoulos@gmail.com>
2024-03-29 23:56:26 +0200

    Added docstrings

--- my_re_functions.py
+++ my_re_functions.py
@@ -2,6 +2,13 @@ import re
 
 
 def get_winner(text):
+    """
+    Extracts the winner of the chess game from the PGN.
+    Args:
+        text(str): The PGN text.
+    Returns
+        str: the winner of the game (ΛΕΥΚΑ, ΜΑΥΡΑ OR ΙΣΟΠΑΛΙΑ)
+    """
     match = re.search(r'\[Result "(.+)"', text)
     if match:
         match = match.group(1)
@@ -14,6 +21,13 @@ def get_winner(text):
 
 
 def get_elo_diff(text):
+    """
+    Extracts the elo rating difference between the two players.
+    Args:
+        text(str): The PGN text.
+    Returns
+        int: The absolute Elo rating difference of the two players.
+    """
     elo_pattern = r'\[WhiteElo "(.+)"', r'\[BlackElo "(.+)"'
     match = re.search(elo_pattern[0], text), re.search(elo_pattern[1], text)
     if match:
@@ -21,6 +35,13 @@ def get_elo_diff(text):
 
 
 def get_date(text):
+    """
+    Extracts the date of the match.
+    Args:
+        text(str): The PGN text.
+    Returns
+        str: The date in a ΗΗ-ΜΜ-ΕΕΕΕ format.
+    """
     date_pattern = r'\[Date "(.+)"'
     match = re.search(date_pattern, text)
     if match:
@@ -30,6 +51,13 @@ def get_date(text):
 
 
 def get_moves(text):
+    """
+    Extracts total moves of the game
+    Args:
+        text(str): The PGN text.
+    Returns
+        int: The total moves played in the game.
+    """
     moves_pattern = r"\d+\.[a-zA-Z]+"
     match = re.findall(moves_pattern, text)
     return len(match)
